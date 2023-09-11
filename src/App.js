8b38ec0ef28e8442ba573c04a3f268f97e48ff57
Jiboy0101 <jaybeesantua1130@gmail.com>
2023-09-11 23:40:12 +0800

    nice

--- src/App.js
+++ src/App.js
@@ -2,24 +2,52 @@ import React, { useState } from 'react';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 import './App.css';
 import iska from './pictures/iska-logo.png';
-
-// Import Font Awesome icons and CSS (you need to include Font Awesome in your project)
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMicrophone, faHome, faCircleQuestion } from '@fortawesome/free-solid-svg-icons';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 export default function DOM() {
   const [speechActive, setSpeechActive] = useState(false);
   const [displayTextOnScreen, setDisplayTextOnScreen] = useState('');
   const [resetButtonVisible, setResetButtonVisible] = useState(false);
+  const [downloadButtonVisible, setDownloadButtonVisible] = useState(false);
+  const [showQuestions, setShowQuestions] = useState(false); // State to control question list visibility
+
+  const generatePDF = () => {
+    const docDefinition = {
+      content: [
+        { text: 'ISKA Displayed Text', style: 'header' },
+        { text: displayTextOnScreen },
+      ],
+      styles: {
+        header: {
+          fontSize: 18,
+          bold: true,
+          margin: [0, 0, 0, 10],
+        },
+      },
+    };
+
+    pdfMake.createPdf(docDefinition).download('iska_displayed_text.pdf');
+  };
 
   const displayText = (text) => {
     let message = new SpeechSynthesisUtterance(text);
     window.speechSynthesis.speak(message);
+    setDownloadButtonVisible(true); // Show the download button
   };
 
   const resetDisplay = () => {
     setDisplayTextOnScreen('');
     setResetButtonVisible(false); // Hide the reset button
+    setDownloadButtonVisible(false); // Hide the download button
+  };
+
+  const toggleQuestions = () => {
+    setShowQuestions(!showQuestions); // Toggle the visibility of question list
   };
 
   const commands = [
@@ -28,8 +56,17 @@ export default function DOM() {
       callback: () => {
         resetTranscript(); // Reset the transcript when a command is executed
         displayText('Here is how to enroll in P U P Lopez, Quezon');
-        setDisplayTextOnScreen('Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.')
+        setDisplayTextOnScreen('Lorem Ipsum is simply dummy text...');
         setResetButtonVisible(true); // Show the reset button after a command is executed
+      },
+    },
+    {
+      command: 'What can you do',
+      callback: () => {
+        resetTranscript();
+        displayText('I can do various things.');
+        setDisplayTextOnScreen('I can help you in every related Questions about PUP.');
+        setResetButtonVisible(true);
       },
     },
   ];
@@ -48,19 +85,32 @@ export default function DOM() {
 
   return (
     <div className="dom-page">
-      <div className="reset-button">
-        {resetButtonVisible && (
-          <button onClick={resetDisplay}>
-            <FontAwesomeIcon icon={faHome} />
-          </button>
-        )}
-        <div className='questions'>
-          <button><FontAwesomeIcon icon={faCircleQuestion} /></button>
+      <div className='top-button'>
+        <div className="reset-button">
+          {resetButtonVisible && (
+            <FontAwesomeIcon onClick={resetDisplay} icon={faHome} size="2xl" />
+          )}
+          <div>
+            <FontAwesomeIcon className='questions' icon={faCircleQuestion} size="2xl" onClick={toggleQuestions} />
+          </div>
         </div>
       </div>
+
+      {showQuestions && (
+        <div className="question-list">
+          {/* Add your list of questions here */}
+          <p>Question 1: How to do XYZ?</p>
+          <p>Question 2: What is ABC?</p>
+          {/* Add more questions as needed */}
+        </div>
+      )}
+
       <img src={iska} alt="PUP Logo" className="logo" />
       <h1 className='app-name'>ISKA</h1>
       <p className='desc'>Hi! I'm ISKA, PUP Virtual Assistant, how can I help you?</p>
+      {downloadButtonVisible && (
+        <button onClick={generatePDF}>Download as PDF</button>
+      )}
       <div className='container'>
         <div className="display-text">
           {displayTextOnScreen && (
@@ -68,6 +118,7 @@ export default function DOM() {
           )}
         </div>
       </div>
+
       <p className="transcript-text">{transcript}</p>
       {speechActive ? (
         <FontAwesomeIcon className='stop' onClick={stopListening} icon={faMicrophone} beat size="sm" style={{"--fa-primary-color": "#ffae00", "--fa-secondary-color": "#ffffff",}} />
